be8d8e6c955373198c472cb6c56e6ef5eb2a1c5c
Quirozdev <luisdevv232@gmail.com>
2023-09-26 17:28:57 -0700

    uploading exercises from 1 to 6

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+let phoneBookEntries = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(phoneBookEntries);
+});
+
+function findPhoneBookEntryByID(id) {
+  const phoneBookEntry = phoneBookEntries.find((phoneBookEntry) => {
+    return phoneBookEntry.id === id;
+  });
+
+  return phoneBookEntry;
+}
+
+function isNameAlreadyUsed(name) {
+  return phoneBookEntries.some((phoneBookEntry) => {
+    return phoneBookEntry.name === name;
+  });
+}
+
+function generateRandomId() {
+  return Math.floor(Math.random() * 100000);
+}
+
+app.get('/api/persons/:id', (req, res) => {
+  const phoneBookEntry = findPhoneBookEntryByID(Number(req.params.id));
+
+  if (!phoneBookEntry) {
+    return res.status(404).send('Phonebook entry not found');
+  }
+
+  res.json(phoneBookEntry);
+});
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body;
+
+  if (!name || !number) {
+    return res.status(400).json({ error: 'Missing name or number' });
+  }
+
+  if (isNameAlreadyUsed(name)) {
+    return res.status(409).json({ error: 'name must be unique' });
+  }
+
+  phoneBookEntries.find((phoneBookEntry) => {
+    return phoneBookEntry.name === name;
+  });
+
+  const newPhoneBookEntry = {
+    name,
+    number,
+    id: generateRandomId(),
+  };
+
+  phoneBookEntries.push(newPhoneBookEntry);
+  res.status(201).json(newPhoneBookEntry);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  phoneBookEntries = phoneBookEntries.filter((phoneBookEntry) => {
+    return phoneBookEntry.id !== id;
+  });
+
+  res.status(204).end();
+});
+
+app.get('/info', (req, res) => {
+  console.log(req);
+  res.send(`
+    <p>Phonebook has info for ${phoneBookEntries.length} people</p>
+    <p>${new Date()}</p>
+  `);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
